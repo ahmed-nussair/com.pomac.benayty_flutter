0f6fa7860d0bc07b6a1112780ede4634105304f2
Ahmed Nussair <anussair@gmail.com>
2020-04-01 15:09:11 +0200

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:flutter/services.dart';
 
 import 'ui/splash.dart';
 
-void main() async {
+void main()  async {
+
+  WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
--- lib/ui/home_pages/home_page.dart
+++ lib/ui/home_pages/home_page.dart
@@ -11,64 +11,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final _items = [
-
-      Item(title: 'مكاتب هندسية', image: 'assets/main_page/engineering_office.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-      Item(title: 'توريد', image: 'assets/main_page/import.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-      Item(title: 'عمالة ومقاولين', image: 'assets/main_page/labour.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-      Item(title: 'حدادة ونجارة وألومنيوم وزجاج', image: 'assets/main_page/carpentry.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-      Item(title: 'سيراميك ورخام وحجر', image: 'assets/main_page/marbles.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-      Item(title: 'مصاعد', image: 'assets/main_page/elevators.png', function: (){},
-        length: MediaQuery.of(context).size.width / 2.3,),
-    ];
     return Provider(
       create: (_) => MainCategoriesService.create(),
       dispose: (_, MainCategoriesService service) => service.client.dispose(),
-      child: FutureBuilder<Response>(
-        future: Provider.of<MainCategoriesService>(context).getMainCategories(),
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.done){
-            final data = json.decode(snapshot.data.bodyString);
-            final List categoriesList = data['data'];
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverPadding(
-                  padding: EdgeInsets.all(8.0),
-                  sliver: SliverGrid.count(
-                    crossAxisCount: 2,
-                    childAspectRatio: .7,
-                    crossAxisSpacing: 5,
-                    mainAxisSpacing: 1,
-                    children: List.generate(categoriesList.length, (index){
-                      return Item(title: categoriesList[index]['name'],
-                        image: categoriesList[index]['imagePath'],
-                        function: (){},
-                        length: MediaQuery.of(context).size.width / 2.3,);
-                    }),
-                  ),
-                )
-              ],
-            );
-          }
-          return Container(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircularProgressIndicator(),
-              ],
-            ),
-          );
-        },
-      ),
+      child: HomePageBody(),
     );
   }
 }
+
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Response>(
+      future: Provider.of<MainCategoriesService>(context).getMainCategories(),
+      builder: (context, snapshot){
+        if(snapshot.connectionState == ConnectionState.done){
+          final data = json.decode(snapshot.data.bodyString);
+          final List categoriesList = data['data'];
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverPadding(
+                padding: EdgeInsets.all(8.0),
+                sliver: SliverGrid.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: .7,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 1,
+                  children: List.generate(categoriesList.length, (index){
+                    return Item(title: categoriesList[index]['name'],
+                      image: categoriesList[index]['imagePath'],
+                      function: (){},
+                      length: MediaQuery.of(context).size.width / 2.3,);
+                  }),
+                ),
+              )
+            ],
+          );
+        }
+        return Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
 
 class Item extends StatelessWidget {
 
@@ -95,7 +88,7 @@ class Item extends StatelessWidget {
           top: 0.0, bottom: 0.0, left: 0.0, right: 0.0,
           child: Column(
             children: <Widget>[
-              Image.asset(image),
+              Image.network(image),
               Text(title,
                 textAlign: TextAlign.center,
                 style: TextStyle(
