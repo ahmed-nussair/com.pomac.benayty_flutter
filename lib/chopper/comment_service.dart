863566c54fb7427555dbedf6890a043e4cd0a756
Ahmed Nussair <anussair@gmail.com>
2020-04-07 16:19:04 +0200

    Initial Commit

---
+++ lib/chopper/comment_service.dart
@@ -0,0 +1,21 @@
+import 'package:chopper/chopper.dart';
+
+part 'comment_service.chopper.dart';
+
+@ChopperApi(baseUrl: '/benaity/public/api/comment')
+abstract class CommentService extends ChopperService {
+  @Post()
+  Future<Response> addComment(@Body() Map<String, dynamic> body);
+
+  static CommentService create() {
+    var client = ChopperClient(
+      baseUrl: 'https://pomac.info',
+      converter: JsonConverter(),
+      services: [
+        _$CommentService(),
+      ],
+    );
+
+    return _$CommentService(client);
+  }
+}
